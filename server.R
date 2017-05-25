9a7b535b5c7321b6678908a98b4967490d4a4c2c
Jon Alkan <alkanj@uw.edu>
2017-05-24 18:15:45 -0700

    adding token for API

--- server.R
+++ server.R
@@ -8,18 +8,18 @@ library(jsonlite)
 setwd("~/Desktop/INFO 201/9-1-1-Incidence-Response-Data")
 #source('')
 #source('')
+
 URI <- "https://data.seattle.gov/resource/pu5n-trf4.json"
+URI.Token <- "urMEfPn6KX0XAeZI6sTjOOAKDKylzmg8rrba"
 response <- GET(URI)
 body <- content(response, "text")
 SPD.data <- fromJSON(body)
 SPD.data <- flatten(SPD.data)
+
+
 
 # Start shinyServer
 shinyServer(function(input, output) { 
   
 })
 
-
-response <- GET("https://api.spotify.com/v1/artists/0oSGxfWSnnOXhD2fKuz2Gy/albums")  # albums by Bowie
-body <- content(response, "text")  # extract the body JSON
-parsed.data <- fromJSON(body)  # convert the JSON string to a list
