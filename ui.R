872bf6e8ddedc438b975b1fac5f48b525aa7cf1c
Emily Zhai <ezhai24@gmail.com>
2017-05-26 20:41:40 -0700

    fixed year slider

--- ui.R
+++ ui.R
@@ -9,33 +9,34 @@ shinyUI(
              
     # tab for interactive map
     tabPanel("Interactive Map",
-      
-      # div to fill screen height
-      div(class = "outer",
-        
-        # use custom style.css file
-        tags$head(
-          includeCSS("style.css")
-        ),
-        
-        # interactive map
-        leafletOutput("incident.map", height = "100%"),
-        
-        # panel for widgets
-        absolutePanel(id = "map-controls", top = 0, left = "auto", right = 40, bottom = 0,
-                      
-          h2("Map Filters"),
-          
-          # filter by date
-          sliderInput("year.slider",
-                      "Year",
-                      min = 2010,
-                      max = strtoi(format(Sys.Date(), "%Y")),
-                      value = c(2016, 2017),
-                      step = 1,
-                      sep = "")
-        )
-      )
+
+       # div to fill screen height
+       div(class = "outer",
+           
+           # use custom style.css file
+           tags$head(
+             includeCSS("style.css")
+           ),
+           
+           # interactive map
+           leafletOutput("incident.map", height = "100%"),
+           
+           # panel for widgets
+           absolutePanel(id = "map-controls", top = 0, left = "auto", right = 40, bottom = 0,
+                         
+             h2("Map Filters"),
+             
+             # filter by date
+             sliderInput("year.slider",
+                         "Year",
+                         min = 2010,
+                         max = strtoi(format(Sys.Date(), "%Y")),
+                         value = c(2016, 2017),
+                         step = 1,
+                         sep = "")
+           )
+       )
+       
     ),
     
     # tab for data breakdown / charts
