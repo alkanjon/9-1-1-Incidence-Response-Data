d4c8f086e8ed107598a2100d4141c0036e14decc
Emily Zhai <ezhai24@gmail.com>
2017-05-26 22:54:23 -0700

    added some comments

--- server.R
+++ server.R
@@ -12,20 +12,15 @@ library(leaflet)
 
 endpoint <- "https://data.seattle.gov/resource/pu5n-trf4.json?"
 app.token <- "ky71McxIFKv1aPgDQr0yM0huK"
-response <- GET(endpoint)
-body <- content(response, "text")
-SPD.data <- fromJSON(body)
-SPD.data <- flatten(SPD.data)
-
-
 
 # Start shinyServer
 shinyServer(function(input, output) {
   
+  # reactive function to adapt to data changes by user
   filteredData <- reactive({
-    # make api request using user defined year range
-    year.query.params <- list("$where" = paste0("event_clearance_date between '", input$year.slider[1], "-01-01T0:00:00' and '", input$year.slider[2], "-12-31T23:59:59'"))
-    response <- GET(endpoint, query = year.query.params)
+    # make api request
+    query.params <- list("$where" = paste0("event_clearance_date between '", input$year.slider[1], "-01-01T0:00:00' and '", input$year.slider[2], "-12-31T23:59:59'"))
+    response <- GET(endpoint, query = query.params)
     body <- content(response, "text")
     yearly.data <- fromJSON(body)
     yearly.data <- flatten(yearly.data)
@@ -34,6 +29,7 @@ shinyServer(function(input, output) {
     yearly.data <- mutate(yearly.data, longitude = as.numeric(longitude), latitude = as.numeric(latitude))
   })
   
+  # render map with default values
   output$incident.map <- renderLeaflet({
     # coerce longitude and latitude to numerics
     yearly.data <- mutate(yearly.data, longitude = as.numeric(longitude), latitude = as.numeric(latitude))
@@ -47,6 +43,7 @@ shinyServer(function(input, output) {
       setView(-122.28, 47.61, zoom = 12)
   })
   
+  # update map as data changes
   observe({
     leafletProxy("incident.map", data = filteredData()) %>%
       clearMarkers() %>%
