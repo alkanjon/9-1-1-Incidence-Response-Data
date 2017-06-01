8e2bdff77b3e7dbdc53df20c2958d7c229781224
Jon Alkan <alkanj@uw.edu>
2017-05-31 18:01:52 -0700

    added insights

--- ui.R
+++ ui.R
@@ -73,10 +73,26 @@ shinyUI(
     
     tabPanel("About the Data",
              h2("Why SPD Data"),
-             p(""),
+             p("We chose to analyze Seattle Police Department data because we wanted to provide a tool that would be beneficial to the
+               thousands of new home buyers entering the Seattle market. Being able to gain quick insights into crime in different 
+               neighborhoods would be a valuable ability to have when deciding where to live in Seattle. Furthermore, being able to 
+               discern crimes on a visual level could help users identify what kind of crimes might apply to them in certain neighborhoods.
+               This could help them make the best decision for them and potentially their families."),
              br(),
              h2("Insights"),
-             p()
+             p("From our analysis we were able to derive insights into the neighborhoods, times, and the crimes most likely to take place
+               in Seattle."),
+             p("The most reported incidences were in regards to traffic, suspisious circumstances, and disturbances. Which tells us that 
+               calls in the Seattle area are, on the surface level, are not dangerous and could to some degree be taken out of consideration
+               when searching for a new home."),
+             p("The times that calls were most frequently called in at were 6PM, 1AM, and 12PM. While this data can be useful to people 
+               looking into where to live, this insight would most likely be more useful for the SPD. Knowing this, the SPD could deploy more
+               patrolls during this hour in areas with higher call rates to reduce response time and most likely deal with violators of the 
+               law in a quicker fashion."),
+             p("Finally, using our data we were able to identify the sectors (SPD Beats) that had the highest number of reported incidents.
+               The two sectors that had the most calls were the K and M sector. Interestingly, both of these sectors are within the West
+               Precint of Seattle. This could help the SPD and current/future homebuyers in the Seattle area identify high incidents
+               areas to aviod.")
     )
   )
 )
